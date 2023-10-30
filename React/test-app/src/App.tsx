28f8e5dec2809cdc707023544fe9281a82f0d7cc
sebHollersbacher <s.hollersbacher@outlook.at>
2023-10-30 21:07:12 +0100

    Changed React app to GET image-names and testImage

--- React/test-app/src/App.tsx
+++ React/test-app/src/App.tsx
@@ -1,28 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 export default function App() {
-  const[result, setResult] = useState([]);
-  const controller = new AbortController();
+  const [result, setResult] = useState([]);
+  const [image, setImage] = useState('');
 
-  useEffect(() => {
-    async function getData() {
-      const response = await fetch('http://192.168.0.27:8000/', {signal: controller.signal});
-      const data = await response.json();
-      setResult(data);
-    }
+  const getImageList = () => {
+    fetch('http://192.168.0.27:8000/files')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        setResult(data);
+      });
+  };
 
-    getData();
-
-    console.log(result, "result");
-
-    return () => {
-      controller.abort();
-    };
-  }, []);
+  const fetchImage = () => {
+    fetch('http://192.168.0.27:8000/uploads/img_1.jpg')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Could not get image');
+        }
+        return response.blob();
+      })
+      .then((blob) => {
+        const imageUrl = URL.createObjectURL(blob);
+        setImage(imageUrl);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   return (
-    <div>test</div>
+    <div>
+    <button onClick={fetchImage}>Get Image</button>
+      <button onClick={getImageList}>Get Image List</button>
+      {result && (
+        <div>
+          <h2>Images:</h2>
+          <pre>{JSON.stringify(result)}</pre>
+        </div>
+      )}
+      {image && <img src={image} alt='img'/>}
+    </div>
   );
 }
