19b1233447be82ca878a983ba27da216250900b4
sebHollersbacher <s.hollersbache@outlook.at>
2023-10-30 21:01:30 +0100

    Added CORS and method to get image names in flask app

--- RaspberryPi/httpFileTest.py
+++ RaspberryPi/httpFileTest.py
@@ -1,10 +1,14 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, send_from_directory
+import glob
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, jsonify
+from flask_cors import CORS, cross_origin
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import ImmutableMultiDict
 from multiprocessing import Value
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # declare counter variable
 counter = Value('i', 0)
@@ -14,6 +18,15 @@ def save_img():
 		counter.value += 1
 		return counter.value
 	# print("Image Saved", end="\n") # debug
+
+    
+@app.route('/files', methods=["GET"])
+@cross_origin()
+def send_files():
+    jpg_files = glob.glob(os.path.join(app.root_path, 'uploads', '*.jpg'))
+    image_names = [os.path.basename(file) for file in jpg_files]
+    return jsonify(image_names)
+
 
 @app.route("/upload-image", methods=["POST"])
 def upload_image():
@@ -33,7 +46,7 @@ def upload_image():
     
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
+    return send_from_directory(os.path.join(app.root_path, "uploads/"),
                                filename)
 
 
